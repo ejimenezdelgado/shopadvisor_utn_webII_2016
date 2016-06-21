584e0bffe42f91cb843285097b549f16247a6b00
Efren Jimenez <efjimenez@itcr.ac.cr>
2016-06-20 20:53:11 -0600

    clase 6 update 3

---
+++ HtmlExtension/HtmlHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace HtmlExtension
+{
+    public static class HtmlHelpers
+    {
+        public static string Label(string target, string text)
+        {
+            return String.Format("<label for='{0}'>{1}</label>", target, text);
+        }
+
+        public static MvcHtmlString Image(this HtmlHelper helper, string src, string altText)
+        {
+            var builder = new TagBuilder("img");
+            builder.MergeAttribute("src", src);
+            builder.MergeAttribute("alt", altText);
+            return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
+        }
+
+
+    }
+}
