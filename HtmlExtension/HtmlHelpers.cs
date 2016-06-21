8556f5d3f70c791a2e3cc1a45a7b76f1d342d529
Efren Jimenez <efjimenez@itcr.ac.cr>
2016-06-21 09:41:34 -0600

    clase 7

--- HtmlExtension/HtmlHelpers.cs
+++ HtmlExtension/HtmlHelpers.cs
@@ -22,6 +22,33 @@ namespace HtmlExtension
             return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
         }
 
+        public static MvcHtmlString ActionLinkSortable(this HtmlHelper helper, 
+            string linkText, string actionName,
+ string sortField, string currentSort, object currentDesc)
+        {
+            bool desc = (currentDesc == null) ? false : Convert.ToBoolean(currentDesc);
+            //get link route values   
+            var routeValues = new System.Web.Routing.RouteValueDictionary();
+            routeValues.Add("id", sortField);
+            routeValues.Add("desc", (currentSort == sortField) && !desc);
+            //build the tag   
+            if (currentSort == sortField) linkText = string.Format("{0} <span class='badge'><span class='glyphicon glyphicon-sort-by-attributes{1}'></span></span>", linkText, (desc) ? "-alt" : "");
+            TagBuilder tagBuilder = new TagBuilder("a");
+            tagBuilder.InnerHtml = linkText;
+            //add url to the link   
+            var urlHelper = new UrlHelper(helper.ViewContext.RequestContext);
+            var url = urlHelper.Action(actionName, routeValues);
+            tagBuilder.MergeAttribute("href", url);
+            //put it all together   
+            return MvcHtmlString.Create(tagBuilder.ToString(TagRenderMode.Normal));
+        }
+
+        public static MvcHtmlString TextboxEmail(this HtmlHelper helper,string value)
+        {
+            string str = "<input type=\"email\" value=\"" + value + "\" />";
+            return new MvcHtmlString(str);
+        }
+
 
     }
 }
